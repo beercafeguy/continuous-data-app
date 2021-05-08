5562c82559983739292fcf35a01e1f59d5f386e9
beercafeguy <he.thuwal08@gmail.com>
2021-05-08 18:51:11 +0530

    Read data in DS

--- hello-spark/src/main/scala/com/beercafeguy/spark/apps/DatasetApp.scala
+++ hello-spark/src/main/scala/com/beercafeguy/spark/apps/DatasetApp.scala
@@ -9,15 +9,25 @@ object DatasetApp {
       .config("spark.sql.shuffle.partitions",4)
       .master("local[*]")
       .getOrCreate()
+
     import spark.implicits._
+    val dataset = Seq(1, 2, 3).toDS()
+    dataset.show()
 
+    val rdd = spark.sparkContext.parallelize(Seq((1, "Spark"), (2, "Databricks")))
+    val integerDS = rdd.toDS()
+    integerDS.show()
 
-    case class Usage(uid:Int,uname:String,usage:Int)
-    val random=new scala.util.Random(42)
-    val data=for (i <- 0 to 1000)
-      yield (Usage(i,"user-"+random.alphanumeric.take(5).mkString(""),random.nextInt(1000)))
+    //case class Person(name: String, age: Int)
 
-    val dsUsage=spark.createDataset(data)
-    dsUsage.show()
+    //val personDS = Seq(Person("Max", 33), Person("Adam", 32), Person("Muller", 62)).toDS()
+    //personDS.show()
+    //case class Usage(uid:Int,uname:String,usage:Int)
+    //val random=new scala.util.Random(42)
+    //val data=for (i <- 0 to 1000)
+    //  yield (Usage(i,"user-"+random.alphanumeric.take(5).mkString(""),random.nextInt(1000)))
+//
+    //val dsUsage=spark.createDataset(data)
+    //dsUsage.show()
   }
 }
