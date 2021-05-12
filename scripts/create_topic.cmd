ddd3f5c64e7939b15caeab5ebbcfae1275c706ba
beercafeguy <he.thuwal08@gmail.com>
2021-05-12 20:52:56 +0530

    Created topic as notifications sink

--- scripts/create_topic.cmd
+++ scripts/create_topic.cmd
@@ -1,1 +1,2 @@
-%KAFKA_HOME%\bin\windows\kafka-topics.bat --create --bootstrap-server localhost:9093 --replication-factor 1 --partitions 1 --topic simple_invoices
+REM %KAFKA_HOME%\bin\windows\kafka-topics.bat --create --bootstrap-server localhost:9093 --replication-factor 1 --partitions 1 --topic simple_invoices
+%KAFKA_HOME%\bin\windows\kafka-topics.bat --create --bootstrap-server localhost:9093 --replication-factor 1 --partitions 1 --topic loyalty_notifications
